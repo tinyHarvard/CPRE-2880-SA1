e86ad16f9a5b11eb6fcf42365e35099a62b7604b
rfeist-EE <rfeist@iastate.edu>
2026-04-28 19:42:47 -0500

    navigation update

---
+++ navigation.h
@@ -0,0 +1,17 @@
+#ifndef NAVIGATION_H_
+#define NAVIGATION_H_
+
+#include "open_interface.h"
+#include "scan.h"
+
+void nav_auto_drive(oi_t *sensor_data, float cm, float deg);
+
+char nav_manual_mode(oi_t *sensor_data);
+
+void nav_bump_avoidance(oi_t *sensor_data, int hit_right, int hit_left);
+
+void boundrydetection(oi_t *sensor_data,
+                      detected_obj_t objects[MAX_OBJECTS],
+                      int smallest_idx);
+
+#endif
